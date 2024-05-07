e49d84df5d2d7f8b4236dc7535bccb5c2668d522
nadzeyakuzmitch <91139114+nadzeyakuzmitch@users.noreply.github.com>
2024-05-07 17:29:09 -0400

    fixed issue #5 (#6)
    squash merge for the logging issue fix

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -63,7 +63,6 @@ class UserService:
             while await cls.get_by_nickname(session, new_nickname):
                 new_nickname = generate_nickname()
             new_user.nickname = new_nickname
-            logger.info(f"User Role: {new_user.role}")
             user_count = await cls.count(session)
             new_user.role = UserRole.ADMIN if user_count == 0 else UserRole.ANONYMOUS            
             if new_user.role == UserRole.ADMIN:
@@ -72,6 +71,7 @@ class UserService:
                 new_user.verification_token = generate_verification_token()
                 await email_service.send_verification_email(new_user)
 
+            logger.info(f"User Role: {new_user.role}")
             session.add(new_user)
             await session.commit()
             return new_user
